bc8b1c37a4ffb1730a5df0eabd61db5fc32a2c85
Chris Maloney <voldrani@gmail.com>
2013-10-19 15:01:30 -0400

    Added placeholders for quadratic and chaining collision detectors

--- TestHashTables.java
+++ TestHashTables.java
@@ -29,34 +29,34 @@ public class TestHashTables {
         System.out.println("Number of test data items = " + testdata.size() + "\n");
 
         CollisionResolver linear = new CollisionResolverLinear();
+        //CollisionResolver quadratic = new CollisionResolverQuadratic();
+        //CollisionResolver chaining = new CollisionResolverChaining();
 
         run_tests("Division module 120, 120 slots, 1 bucket / slot, linear probing",
                   new HashTableOneBucket(HashFunction.div_mod_120, 120, linear));
         //run_tests("Division module 120, 120 slots, 1 bucket / slot, quadratic probing",
-        //          new HashTableOneBucket(HashFunction.div_mod_120, 120, 1));
+        //          new HashTableOneBucket(HashFunction.div_mod_120, 120, quadratic));
         //run_tests("Division module 120, 120 slots, 1 bucket / slot, in-table chaining",
-        //          new HashTableOneBucket(HashFunction.div_mod_120, 120, 2));
+        //          new HashTableOneBucket(HashFunction.div_mod_120, 120, chaining));
 
         run_tests("Division module 127, 120 slots, 1 bucket / slot, linear probing",
                   new HashTableOneBucket(HashFunction.div_mod_127, 120, linear));
         //run_tests("Division module 127, 120 slots, 1 bucket / slot, quadratic probing",
-        //          new HashTableOneBucket(HashFunction.div_mod_127, 120, 1));
+        //          new HashTableOneBucket(HashFunction.div_mod_127, 120, quadratic));
         //run_tests("Division module 127, 120 slots, 1 bucket / slot, in-table chaining",
-        //          new HashTableOneBucket(HashFunction.div_mod_127, 120, 2));
+        //          new HashTableOneBucket(HashFunction.div_mod_127, 120, chaining));
 
         run_tests("Division module 41, 40 slots, 3 buckets / slot, linear probing",
                   new HashTableMultiBucket(HashFunction.div_mod_41, 120, 3, linear));
         //run_tests("Division module 41, 40 slots, 3 buckets / slot, quadratic probing",
-        //          new HashTableMultiBucket(HashFunction.div_mod_41, 120, 3, 1));
-        //run_tests("Division module 41, 40 slots, 3 buckets / slot, in-table chaining",
-        //          new HashTableMultiBucket(HashFunction.div_mod_41, 120, 3, 2));
+        //          new HashTableMultiBucket(HashFunction.div_mod_41, 120, 3, quadratic));
 
         run_tests("Custom hash function, 120 slots, 1 bucket / slot, linear probing",
                   new HashTableOneBucket(HashFunction.custom, 120, linear));
         //run_tests("Custom hash function, 120 slots, 1 bucket / slot, quadratic probing",
-        //          new HashTableOneBucket(HashFunction.custom, 120, 1));
+        //          new HashTableOneBucket(HashFunction.custom, 120, quadratic));
         //run_tests("Custom hash function, 120 slots, 1 bucket / slot, in-table chaining",
-        //          new HashTableOneBucket(HashFunction.custom, 120, 2));
+        //          new HashTableOneBucket(HashFunction.custom, 120, chaining));
     }
 
     public static void run_tests(String title, HashTable table) {
